c84426e9afdfdd01f3211f601164ed1159628699
Konrad <konrad.hanus@gmail.com>
2019-03-21 19:43:46 +0100

    Template Login and SignIn

--- react-firebase-auth/src/Components/DefaultLoginTemplate/index.jsx
+++ react-firebase-auth/src/Components/DefaultLoginTemplate/index.jsx
@@ -1,17 +1,29 @@
 import React from "react";
+import "./style.css";
 const loginTemplate = () => {
-    return (
-      <div>
-        <div>
-          <label>Login</label>
-          <input name="login" />
+  return (
+    <div className="rfa-default-login-template">
+      <div className="rfa-default-login-template__title">Login</div>
+      <div className="rfa-default-login-template__body">
+        <div className="rfa-default-login-template__row">
+          <div className="rfa-default-login-template__cell">
+            <label>Login:</label>
+          </div>
+          <div className="rfa-default-login-template__cell">
+            <input name="login" />
+          </div>
         </div>
-        <div>
-          <label>Password</label>
-          <input name="password" type="password" />
+        <div className="rfa-default-login-template__row">
+          <div className="rfa-default-login-template__cell">
+            <label>Password:</label>
+          </div>
+          <div className="rfa-default-login-template__cell">
+            <input name="password" type="password" />
+          </div>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default loginTemplate;
+export default loginTemplate;
--- react-firebase-auth/src/Components/DefaultSignInTemplate/index.jsx
+++ react-firebase-auth/src/Components/DefaultSignInTemplate/index.jsx
@@ -1,21 +1,36 @@
 import React from "react";
+import "./style.css";
 const signInTemplate = () => {
-    return (
-      <div>
-        <div>
-          <label>Login</label>
-          <input name="login" />
+  return (
+    <div className="rfa-default-sign-in-template">
+    <div className="rfa-default-sign-in-template__title">SignIn</div>
+      <div className="rfa-default-sign-in-template__body">
+        <div className="rfa-default-sign-in-template__row">
+          <div className="rfa-default-sign-in-template__cell">
+            <label>Login</label>
+          </div>
+          <div className="rfa-default-sign-in-template__cell">
+            <input name="login" />
+          </div>
         </div>
-        <div>
+        <div className="rfa-default-sign-in-template__row">
+          <div className="rfa-default-sign-in-template__cell">
           <label>Password</label>
+          </div>
+          <div className="rfa-default-sign-in-template__cell">
           <input name="password" type="password" />
+          </div>
         </div>
-        <div>
+        <div className="rfa-default-sign-in-template__row">
+          <div className="rfa-default-sign-in-template__cell">
           <label>RePassword</label>
+          </div>
+          <div className="rfa-default-sign-in-template__cell">
           <input name="rePassword" type="password" />
+          </div>
         </div>
       </div>
-    );
-  };
-  export default signInTemplate;
-  
+    </div>
+  );
+};
+export default signInTemplate;
